5c587fc887cc07a2ccbd829b63aaa7355d25cda5
LiiNen <kjeonghoon065@gmail.com>
2021-08-07 14:59:27 +0900

    Add connection & results parsing

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 require("dotenv").config();
-console.log(process.env.MYSQL_HOST);
-console.log(process.env.MYSQL_USER);
-console.log(process.env.MYSQL_PASSWORD);
-console.log(process.env.MYSQL_DATABASE);
 
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: MYSQL_HOST,
-    user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
 })
 
 connection.connect();
+
+connection.query('select * from a', function(error, results, fields) {
+    if(error) console.log(error);
+    else {
+        console.log(results);
+        results.map(function(packet) {
+            console.log('------');
+            console.log('packet.id: ', packet.id);
+            console.log('packet.name: ', packet.name);
+        });
+    }
+});
 
 connection.end();
