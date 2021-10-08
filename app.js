7780479c148d07c4280c946e7ccde59dacfecf28
LiiNen <kjeonghoon065@gmail.com>
2021-10-08 22:23:34 +0900

    Hack for friend // have to check request

--- app.js
+++ app.js
@@ -514,6 +514,52 @@ app.delete('/playlist/item', (req, res) => {
     });
 });
 
+
+/**
+ * /friend
+ * [GET] user_id => get friends list(id, name)
+ * [POST] user_id, email => add friend state (user_id for apply, email for response user)
+ */
+app.get('/friend', (req, res) => {
+    var user_id = req.query.user_id;
+    var query = `select friend_id, accept from (
+                (select user_id_response as friend_id, accept from friend where user_id_apply=${user_id})
+                join (select user_id_apply as friend_id, accept from friend where user_id_response=${user_id}));`;
+    connection.query(query, function(error, results, fields) {
+        if(error) {
+            console.log(error);
+            res.json(query_error);
+        }
+        else {
+            console.log(results);
+            res.json({
+                'status': true,
+                'body': results
+            });
+        }
+    });
+});
+
+app.post('/friend', (req, res) => {
+    var user_id = req.body.user_id;
+    var email = req.body.email;
+    var query = `insert into friend(user_id_apply, user_id_response, accept) values(user_id, (select id from user where user.email=${email}), false);`;
+    connection.query(query, function(error, results, fields) {
+        if(error) {
+            console.log(error);
+            res.json(query_error);
+        }
+        else {
+            console.log(results);
+            res.json({
+                'status': true,
+                'body': 'friend apply success'
+            });
+        }
+    });
+});
+
+
 /**
  * start express server
  */
