3e52223d905fcb8bc7288ee3f439b955f6c9ff12
LiiNen <kjeonghoon065@gmail.com>
2021-10-10 02:20:20 +0900

    Fix friend api & add curation to playlist api

--- routes/playlist.js
+++ routes/playlist.js
@@ -3,6 +3,9 @@
  * [GET] user_id => get playlist of userid
  * [POST] user_id, title, visible => create playlist with userid
  * [DELETE] id => delete playlist of id(playlist)
+ * 
+ * /playlist/curation
+ * [POST] user_id, curation_id => curation data to user's playlist (include _item curation -> playlist)
  */
 
 module.exports = (connection) => {
@@ -59,6 +62,34 @@ module.exports = (connection) => {
         res.json({
             'status': true,
             'body': 'delete success'
+        });
+      }
+    });
+  });
+
+  router.post('/curation', (req, res) => {
+    var user_id = req.body.user_id;
+    var curation_id = req.body.curation_id;
+
+    var query = `insert into playlist(user_id, title) select ${user_id}, title from curation where id=${curation_id};`;
+    connection.query(query, function(error, results, fields) {
+      if(error) {
+        console.log(error);
+        res.json('error');
+      }
+      else {
+        var query_item = `insert into playlist_item(playlist_id, music_id) select ${results.insertId}, music_id from curation_item where curation_id=${curation_id};`;
+        connection.query(query_item, function(error, results, fields) {
+          if(error) {
+            console.log(error);
+            res.json('error in item');
+          }
+          else {
+            res.json({
+              'status': true,
+              'body': 'insert success'
+            });
+          }
         });
       }
     });
