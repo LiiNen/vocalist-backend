b7593f139747d109e2099b33ebdf899b2a31db2e
LiiNen <kjeonghoon065@gmail.com>
2021-10-25 19:17:41 +0900

    Add bugReport get & append column email

--- routes/bugReport.js
+++ routes/bugReport.js
@@ -2,12 +2,35 @@ module.exports = () => {
   var router = require('express').Router();
   var getConnection = require('../connection');
 
+  router.get('/', (req, res) => {
+    var query = `select user_id, br.email, name, title, content from bug_report as br, user where br.user_id = user.id`;
+
+    getConnection(function(connection) {
+      connection.query(query, function(error, results, fields) {
+        if(error) {
+          res.json({
+            'status': false,
+            'log': 'query error'
+          });
+        }
+        else {
+          res.json({
+            'status': true,
+            'body': results
+          });
+        }
+      });
+      connection.release();
+    })
+  });
+
   router.post('/', (req, res) => {
     var user_id = req.body.user_id;
     var title = req.body.title;
     var content = req.body.content;
-    var query = `insert into bug_report(user_id, title, content)
-                values(${user_id}, \"${title}\", \"${content}\")`;
+    var email = req.body.email;
+    var query = `insert into bug_report(user_id, title, content, email)
+                values(${user_id}, \"${title}\", \"${content}\", \"${email}\")`;
 
     getConnection(function(connection) {
       connection.query(query, function(error, results, fields) {
