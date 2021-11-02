f29fdc70a643de2f9bff92a3ac66b8298a7b0b53
LiiNen <kjeonghoon065@gmail.com>
2021-11-02 17:26:30 +0900

    Add user hack api

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,54 @@ module.exports = () => {
   var router = require('express').Router();
   var getConnection = require('../connection');
 
+  router.get('/', (req, res) => {
+    var id = req.query.id;
+    var query = `select * from user where id=${id}`;
+
+    getConnection(function(connection) {
+      connection.query(query, function(error, results, fields) {
+        if(error) {
+          console.log(error);
+          res.json({
+            'status': false,
+            'log': 'query error'
+          });
+        }
+        else {
+          res.json({
+            'status': true,
+            'body': results[0]
+          })
+        }
+      });
+      connection.release();
+    });
+  });
+
+  router.post('/', (req, res) => {
+    var emoji = req.body.emoji;
+    var query = `insert into user(email, name, type, emoji) values("test@ac.kr", "test", "google", \"${emoji}\")`;
+
+    getConnection(function(connection) {
+      connection.query(query, function(error, results, fields) {
+        if(error) {
+          console.log(error);
+          res.json({
+            'status': false,
+            'log': 'query error'
+          });
+        }
+        else {
+          res.json({
+            'status': true,
+            'body': 'select fin'
+          })
+        }
+      });
+      connection.release();
+    });
+  });
+
   router.patch('/', (req, res) => {
     var id = req.body.id;
     var name = req.body.name;
