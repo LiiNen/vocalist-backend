f56e9485b6e357592ffd90438774b99d5b4c6dea
LiiNen <kjeonghoon065@gmail.com>
2021-11-15 14:29:46 +0900

    Add patch list for pitch

--- routes/love.js
+++ routes/love.js
@@ -92,7 +92,7 @@ module.exports = () => {
 
   router.get('/list', (req, res) => {
     var user_id = req.query.user_id;
-    var target = 'music.id, music.title, music.artist, 1 as islike';
+    var target = 'music.id, music.title, music.artist, 1 as islike, love.pitch';
     var query = `select ${target} from music, love where music.id = love.music_id and love.user_id = ${user_id}`;
 
     getConnection(function(connection) {
@@ -110,7 +110,33 @@ module.exports = () => {
           });
         }
       });
-    })
+      connection.release();
+    });
+  });
+
+  router.patch('/list/pitch', (req, res) => {
+    var user_id = req.query.user_id;
+    var music_id = req.query.music_id;
+    var pitch = req.query.pitch;
+    var query = `update love set pitch = ${pitch} where user_id=${user_id} and music_id=${music_id};`;
+
+    getConnection(function(connection) {
+      connection.query(query, function(error, results, fields) {
+        if(error) {
+          res.json({
+            'status': false,
+            'log': 'query error'
+          });
+        }
+        else {
+          res.json({
+            'status': true,
+            'body': 'set pitch success'
+          });
+        }
+      });
+      connection.release();
+    });
   });
   
   return router;
