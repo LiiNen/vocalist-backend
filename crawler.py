bfe70927c95e04f72b40870f13073e19454776fb
LiiNen <kjeonghoon065@gmail.com>
2022-01-31 14:38:24 +0900

    Add new music, movie crawling code

--- crawler.py
+++ crawler.py
@@ -9,18 +9,34 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 from dotenv import load_dotenv
 
-
-chart_data_list = []
 load_dotenv(verbose=True)
-TJ_SRC=os.getenv('TJ_CHART')
+
+TJ_CHART_SRC=os.getenv('TJ_CHART')
+TJ_NEW_SRC=os.getenv('TJ_NEW')
+MOVIE_URL=os.getenv('MOVIE_URL')
+SEARCH_BASE=os.getenv('SEARCH_BASE')
 POST_URL=os.getenv('POST_URL')
 PATCH_URL=os.getenv('PATCH_URL')
 
-def init_chart_data():
-  chart_data_list = []
+music_data_list = []
+error = 0
+patch = 0
+post = 0
+exist = 0
 
-def add_chart_row(param):
-  chart_data_list.append({
+
+def init_chart_data():
+  global music_data_list, error, patch, post, exist
+  music_data_list = []
+  error = 0
+  patch = 0
+  post = 0
+  exist = 0
+
+
+def add_row(param):
+  global music_data_list
+  music_data_list.append({
     'chart': param[0],
     'number': param[1],
     'title': param[2],
@@ -29,21 +45,39 @@ def add_chart_row(param):
     'isMV': param[5]
   })
 
+
 def get_chart():
-  driver.get(TJ_SRC)
+  init_chart_data()
+
+  driver.get(TJ_CHART_SRC)
   chart_list = driver.find_elements(By.CSS_SELECTOR, '#BoardType1 tbody tr')
   chart_list.pop(0)
 
   for chart in chart_list:
     chart_td = chart.find_elements(By.CSS_SELECTOR, 'td')
-    add_chart_row([chart_td[0].text, chart_td[1].text, chart_td[2].text, chart_td[3].text, False, False])
+    add_row([chart_td[0].text, chart_td[1].text, chart_td[2].text, chart_td[3].text, False, False])
+  
+  post_data()
 
-def post_chart():
+
+def get_new():
+  init_chart_data()
+
+  driver.get(TJ_NEW_SRC)
+  new_list = driver.find_elements(By.CSS_SELECTOR, '#BoardType1 tbody tr')
+  new_list.pop(0)
+  
+  for new in new_list:
+    new_td = new.find_elements(By.CSS_SELECTOR, 'td')
+    add_row([-1, new_td[0].text, new_td[1].text, new_td[2].text, False, False])
+
+  post_data()
+
+
+def post_data():
+  global music_data_list, error, patch, post, exist
   res = requests.patch(PATCH_URL)
-  error = 0
-  patch = 0
-  post = 0
-  for chart_data in chart_data_list:
+  for chart_data in music_data_list:
     request_body = {
       'chart': chart_data['chart'],
       'number': chart_data['number'],
@@ -56,12 +90,45 @@ def post_chart():
     if(res.json()['status'] == False):
         error = error + 1
     else:
-        if(res.json()['body'] == 'patch'):
-            patch = patch + 1
-        if(res.json()['body'] == 'post'):
-            post = post + 1
-    if (error+patch+post == 100):
-      print(error, patch, post)
+      if(res.json()['body'] == 'patch'):
+        patch = patch + 1
+      if(res.json()['body'] == 'post'):
+        post = post + 1
+      if(res.json()['body'] == 'exist'):
+        exist = exist + 1
+    if (error+patch+post+exist == len(music_data_list)):
+      print(error, patch, post, exist)
+
+def get_movie():
+  res = requests.get(MOVIE_URL)
+  empty_movie_list = res.json()['body']
+  request_body_list = []
+  for empty_movie in empty_movie_list:
+    count = 0
+    while(True):
+      try:
+        driver.get(SEARCH_BASE + empty_movie['artist'] + '+' + empty_movie['title'])
+        code = driver.find_elements(By.CSS_SELECTOR, 'a#video-title')[0].get_attribute('href')
+        request_body_list.append([empty_movie['id'], code.split('watch?v=')[1]])
+        break
+      except:
+        if(count > 10):
+            break
+        count += 1
+        continue
+  success = 0
+  error = 0
+  for request_body in request_body_list:
+      datas = {
+          'id': request_body[0],
+          'code': request_body[1]
+      }
+      res = requests.patch(MOVIE_URL, json=datas)
+      if(res.json()['status'] == False):
+          error = error + 1
+      else:
+          success = success + 1
+  print('movie patch: ', error, success)
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
@@ -70,8 +137,8 @@ options.add_argument("disable-gpu")
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-init_chart_data()
+get_movie()
+get_new()
 get_chart()
-post_chart()
 
 driver.quit()
