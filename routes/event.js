206b6003b53e7b8af6ede64cbc24bb8854772b04
LiiNen <kjeonghoon065@gmail.com>
2022-03-16 21:55:50 +0900

    Fix event query errors

--- routes/event.js
+++ routes/event.js
@@ -19,7 +19,7 @@ module.exports = () => {
         else {
           res.json({
             'status': true,
-            'body': results
+            'body': results[0]
           });
         }
       });
@@ -29,7 +29,7 @@ module.exports = () => {
 
   router.get('/user', (req, res) => {
     var user_id = req.query.user_id;
-    var query = `select participate, ad_participate from event where user_id=? and id > 1`;
+    var query = `select participate, ad_participate, phone from event where user_id=? and id > 1`;
 
     getConnection(function(connection) {
       connection.query(query, [user_id], function(error, results, fields) {
@@ -43,7 +43,7 @@ module.exports = () => {
           if(results.length == 0) {
             res.json({
               'status': true,
-              'body': {"phone": '0', "participate": 0,"ad_participate": 0}
+              'body': {"phone": '', "participate": 0,"ad_participate": 0}
             });
           }
           else {
@@ -59,11 +59,12 @@ module.exports = () => {
   });
 
   router.post('/user', (req, res) => {
-    var user_id = req.query.user_id;
-    var query = `insert into
-                event(user_id, participate, ad_participate, phone)
-                values(?, 1, 0, ?)`;
-    var params = [user_id, phone];
+    var user_id = req.body.user_id;
+    var phone = req.body.phone;
+    var query = `insert into event(user_id, participate, ad_participate, phone)
+                select ?, 1, 0, ? from dual
+                where not exists (select user_id from event where user_id=?)`;
+    var params = [user_id, phone, user_id];
     
     getConnection(function(connection) {
       connection.query(query, params, function(error, results, fields) {
@@ -85,8 +86,8 @@ module.exports = () => {
   });
 
   router.patch('/user', (req, res) => {
-    var user_id = req.query.user_id;
-    var query = `update event ad_participate=1 where user_id=?`;
+    var user_id = req.body.user_id;
+    var query = `update event set ad_participate=1 where user_id=?`;
     var params = [user_id];
 
     getConnection(function(connection) {
